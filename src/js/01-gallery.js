6d87a6cd803ddf5e6bd9bbbef408a17dd515c7a9
vladbevz <bevz.vlad15@gmail.com>
2023-07-06 21:37:07 +0200

    task 1 update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+// etap 1
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const li = document.querySelectorAll('li');
+createImagesItems();
+
+function createImagesItems() {
+  const importImages = createImagesEl(galleryItems);
+  galleryList.innerHTML = importImages.join('');
+}
+function createImagesEl(items) {
+  return items.map(
+    item => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>
+  `
+  );
+}
+
+const lightbox = new SimpleLightbox('.gallery a');
