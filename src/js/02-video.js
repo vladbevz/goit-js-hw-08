530cc77ce7ac46ea507705aaa1a48226e56745fa
vladbevz <bevz.vlad15@gmail.com>
2023-07-06 22:11:05 +0200

    task 2 update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const TIME_CURRENT = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(event) {
+  const currentTime = event.seconds;
+  localStorage.setItem(TIME_CURRENT, currentTime);
+}
+
+const storedTime = localStorage.getItem(TIME_CURRENT);
+if (storedTime) {
+  player.setCurrentTime(storedTime);
+}
